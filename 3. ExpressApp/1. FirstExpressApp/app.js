afaa8fe5a9ecac1e444d459e733e784fa18df14d
Phyllis Kan <Phyllis Kan>
2020-07-17 23:51:17 -0500

    intro to route params and "*"

--- 3. ExpressApp/1. FirstExpressApp/app.js
+++ 3. ExpressApp/1. FirstExpressApp/app.js
@@ -19,6 +19,27 @@ app.get("/dog", function (req, res) {
     res.send("MEOW!")
 })
 
+// route params
+app.get("/r/:subredditName", function (req, res) {
+
+    let subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+})
+
+
+app.get("/r/:subredditName/comments/:id/:title", function (req, res) {
+    console.log(req.params);
+    // will give out 3 params
+    res.send("WELCOME TO THE COMMENTS PAGE!");
+})
+
+
+// "*" => any url aside from the one already defined
+// it matters the order you put it
+app.get("*", function (req, res) {
+    res.send("YOU ROCKS!")
+})
+
 // get the port
 app.listen(3000, function () {
     console.log("server has started")
