7b5135a21194969bad8bf55702722426e7dc8c72
Phyllis Kan <Phyllis Kan>
2020-07-22 13:37:34 -0500

    adding partial for styling

--- 4. MoreExpress/1. IntroToEJS/app.js
+++ 4. MoreExpress/1. IntroToEJS/app.js
@@ -1,16 +1,19 @@
 let express = require("express")
 let app = express()
 
+app.use(express.static("public")) // tell Express to serve <any> directory other than views
+app.set("view engine", "ejs") // so don't need to type in ejs for all view files passed in in res.render
+
 // ejs = embedded js, has to be in "view" directory
 
 // use "render" if we are pulling up files 
 app.get("/", function (req, res) {
-    res.render("home.ejs") // will look in views directory
+    res.render("home") // will look in views directory
 })
 
 app.get("/fallinlovewith/:thing", function (req, res) {
     let what = req.params.thing;
-    res.render("love.ejs", { whatPassedIn: what })
+    res.render("love", { whatPassedIn: what })
 })
 
 app.get("/posts", function (req, res) {
@@ -19,7 +22,7 @@ app.get("/posts", function (req, res) {
         { title: "How I meet my first ferret", author: "Kan" },
         { title: "Working at first job", author: "Phil" }
     ]
-    res.render("posts.ejs", { postsAppear: posts })
+    res.render("posts", { postsAppear: posts })
 })
 
 app.listen(3000, function () {
