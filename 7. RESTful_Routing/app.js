153f2ef3f080f25931af501c4b1468733f41a6b5
Phyllis Kan <pyk9q7@gmail.com>
2020-08-09 21:24:39 -0500

    intro to method override & add EDIT/ UPDATE routes

--- 7. RESTful_Routing/app.js
+++ 7. RESTful_Routing/app.js
@@ -2,12 +2,14 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const methodOverride = require("method-override")
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true, useUnifiedTopology: true })
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride("_method")) // Reminder: ?_method=PUT"
 
 // MONGOOSE/MODEL CONFIG
 let blogSchema = new mongoose.Schema({
@@ -23,7 +25,7 @@ let Blog = mongoose.model("Blog", blogSchema)
 app.get("/", function (req, res) {
     res.redirect("/blogs")
 })
-
+// INDEX
 app.get("/blogs", function (req, res) {
     Blog.find({}, function (err, blogs) {
         if (err) {
@@ -34,10 +36,12 @@ app.get("/blogs", function (req, res) {
     })
 })
 
+// NEW
 app.get("/blogs/new", function (req, res) {
     res.render("new")
 })
 
+//CREATE
 app.post("/blogs", function (req, res) {
     Blog.create(req.body.blog, function (err, newBlog) {
         if (err) {
@@ -48,12 +52,36 @@ app.post("/blogs", function (req, res) {
     })
 })
 
+//SHOW
 app.get("/blogs/:id", function (req, res) {
     Blog.findById(req.params.id, function (err, foundBlog) {
         if (err) {
             res.redirect("/blogs")
         } else {
             res.render("show", { blog: foundBlog })
+        }
+    })
+})
+
+// EDIT
+app.get("/blogs/:id/edit", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs")
+        } else {
+            res.render("edit", { blog: foundBlog })
+        }
+    })
+})
+
+// UPDATE 
+app.put("/blogs/:id", function (req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
+        //id, newData, callback
+        if (err) {
+            res.redirect("/blogs")
+        } else {
+            res.redirect("/blogs/" + req.params.id)
         }
     })
 })
