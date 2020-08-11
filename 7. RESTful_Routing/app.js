00209c8eb4e994fbed6c05fc47851e1c0c009247
Phyllis Kan <pyk9q7@gmail.com>
2020-08-11 18:48:25 -0500

    adding express-sanitizer package

--- 7. RESTful_Routing/app.js
+++ 7. RESTful_Routing/app.js
@@ -3,13 +3,16 @@ const app = express()
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const methodOverride = require("method-override")
+const expressSanitizer = require("express-sanitizer")
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true, useUnifiedTopology: true })
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(expressSanitizer()) // to prevent users including script tag in the webpage
 app.use(methodOverride("_method")) // Reminder: ?_method=PUT"
+
 
 // MONGOOSE/MODEL CONFIG
 let blogSchema = new mongoose.Schema({
@@ -43,6 +46,10 @@ app.get("/blogs/new", function (req, res) {
 
 //CREATE
 app.post("/blogs", function (req, res) {
+    // sanitizing
+    // req.body.blog.body = req.sanitize(req.body.blog.body)
+    // req body is whatever coming from the form
+    // blog.body is our blog[body]
     Blog.create(req.body.blog, function (err, newBlog) {
         if (err) {
             res.render("new")
@@ -76,6 +83,8 @@ app.get("/blogs/:id/edit", function (req, res) {
 
 // UPDATE 
 app.put("/blogs/:id", function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
         //id, newData, callback
         if (err) {
